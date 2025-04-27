722f2622aa0ab1fd7ed54a4c6497c6016cf8fa77
Aryan Bachchu <aryanbachchu150@gmail.com>
2025-04-28 01:31:48 +0530

    cd fix iteration-2c

--- apps/http-server/index.ts
+++ apps/http-server/index.ts
@@ -1,8 +1,9 @@
 import express from "express";
+import cors from "cors";
 import { client } from "@repo/db/client";
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 app.get("/users", (req, res) => {
